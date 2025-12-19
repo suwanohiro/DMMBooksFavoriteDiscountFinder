655d6fab9603e7a24a8223dbff1b311a1356a29a
suwanohiro <supermariomaker1110@gmail.com>
2025-12-20 00:55:21 +0900

    リセットボタン実装

--- src/gui/MainMenu/js/script.js
+++ src/gui/MainMenu/js/script.js
@@ -12,6 +12,47 @@ function main() {
     syncRange("swn-percent-min");
     syncRange("swn-return-min");
     syncRange("swn-return-max");
+
+    resetValues();
+
+    const swnContentActive = document.getElementsByClassName("swn-content-active");
+    Array.from(swnContentActive).forEach(elem => {
+        elem.addEventListener("input", () => {
+            const titleDiv = elem.closest('.swn-content-title');
+            const parentElem = titleDiv.nextElementSibling.nextElementSibling;
+            const parentResetButton = parentElem.querySelector('.swn-content-value-reset');
+            parentResetButton.disabled = !elem.checked;
+            if (elem.checked) {
+                parentElem.classList.remove("swn-disabled");
+            } else {
+                parentElem.classList.add("swn-disabled");
+            }
+        });
+    });
+}
+
+function resetValues() {
+    const resetButtons = document.getElementsByClassName("swn-content-value-reset");
+    Array.from(resetButtons).forEach(button => {
+        // minとmaxの要素を取得
+        const parentElem = button.parentElement;
+        const rangeInputs = parentElem.getElementsByTagName("input");
+        const minInput = Array.from(rangeInputs).find(input => input.type === "range" && input.id.endsWith("min"));
+        const maxInput = Array.from(rangeInputs).find(input => input.type === "range" && input.id.endsWith("max"));
+        const minDefaultValue = minInput ? minInput.defaultValue : null;
+        const maxDefaultValue = maxInput ? maxInput.defaultValue : null;
+
+        button.addEventListener("click", () => {
+            if (minInput && maxInput) {
+                minInput.value = minDefaultValue;
+                maxInput.value = maxDefaultValue;
+                const minTxt = document.getElementById(`${minInput.id}-txt`);
+                const maxTxt = document.getElementById(`${maxInput.id}-txt`);
+                if (minTxt) minTxt.innerHTML = minDefaultValue;
+                if (maxTxt) maxTxt.innerHTML = maxDefaultValue;
+            }
+        });
+    });
 }
 
 main();
